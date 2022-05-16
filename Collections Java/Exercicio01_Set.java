7819298be9f1726cca142616e56303c17eed9cb4
Arruda-Dan <58825730+Arruda-Dan@users.noreply.github.com>
2022-05-16 16:52:33 -0300

    Add files via upload
    Adicionando exercícios. Falta implementá-los

---
+++ Collections Java/Exercicio01_Set.java
@@ -0,0 +1,38 @@
+package desafios.dio.collections.set;
+
+public class Exercicio01_Set {
+
+	public static void main(String[] args) {
+		
+		/*
+		 *   Exercícios Propostos
+		 *   
+		 *   01. 
+		 *   Crie um conjunto contendo as cores do arco-íris e:
+		 *   1. Exiba todas as cores uma abaixo da outra
+		 *   2. A quantidade de cores que o arco-íris tem
+		 *   3. Exiba as cores em ordem alfabética
+		 *   4. Exiba as cores na ordem inversa da que foi informada
+		 *   5. Exiba todas as cores que começam com a letra ”v”
+		 *   6. Remova todas as cores que não começam com a letra “v”
+		 *   Limpe o conjunto
+		 *   Confira se o conjunto está vazio
+		 *   
+		 *   02. 
+		 *   Crie uma classe LinguagemFavorita que possua os atributos nome,
+		 *   anoDeCriacao e ide. Em seguida, crie um conjunto com 3 linguagens
+		 *   e faça um programa que ordene esse conjunto por:
+		 *   1. Ordem de Inserção
+		 *   2. Ordem Natural (nome)
+		 *   3. IDE
+		 *   4. Ano de criação e nome
+		 *   5. Nome, ano de criacao e IDE
+		 *   Ao final, exiba as linguagens no console, um abaixo da outra.
+		 *   
+		 */
+		
+
+
+	}
+	
+}
