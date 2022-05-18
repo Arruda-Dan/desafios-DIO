3481a05a46d410d014cc01586fcf7ecad33a5574
Arruda-Dan <58825730+Arruda-Dan@users.noreply.github.com>
2022-05-18 12:05:39 -0300

    Add files via upload
    implementando código

--- Collections Java/Exercicio01_Set.java
+++ Collections Java/Exercicio01_Set.java
@@ -1,38 +1,82 @@
 package desafios.dio.collections.set;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
 public class Exercicio01_Set {
 
+	/*
+	 * Exercícios Propostos
+	 * 
+	 * 01. Crie um conjunto contendo as cores do arco-íris e: 
+	 * 1. Exiba todas as cores uma abaixo da outra 
+	 * 2. A quantidade de cores que o arco-íris tem 
+	 * 3. Exiba as cores em ordem alfabética 
+	 * 4. Exiba as cores na ordem inversa da que foi informada 
+	 * 5. Exiba todas as cores que começam com a letra ”v” 
+	 * 6. Remova todas as cores que não começam com a letra “v” 
+	 * Limpe o conjunto 
+	 * Confira se o conjunto está vazio
+	 */
+
 	public static void main(String[] args) {
+
+		//Adicionando cores do arcoIris
+		List<String> arcoIris = new ArrayList<String>();
+		arcoIris.add("Vermelho");
+		arcoIris.add("Laranja");
+		arcoIris.add("Amarelo");
+		arcoIris.add("Verde");
+		arcoIris.add("Azul");
+		arcoIris.add("Anil");
+		arcoIris.add("Violeta");
+
+		//Imprimindo as cores uma abaixo da outra + Contador
+		int quantidadeCores = 0;
+		for (int i = 0; i < arcoIris.size(); i++) {
+			System.out.println(arcoIris.get(i) + " ");
+			quantidadeCores++;
+		}
+
+		//Imprimindo a quantidade de elementos da lista
+		System.out.println();
+		System.out.println("Total de cores: " + quantidadeCores);
+
+		//Cores em ordem alfabética
+		Collections.sort(arcoIris);
+		System.out.println();
+		System.out.println(arcoIris);
+
+		//Imprimindo só os elementos que começam com V
+		System.out.println();
+		for (String string : arcoIris) {
+			char carac = string.charAt(0);
+			if (carac == 'V') System.out.print(string + " ");
+		}
 		
-		/*
-		 *   Exercícios Propostos
-		 *   
-		 *   01. 
-		 *   Crie um conjunto contendo as cores do arco-íris e:
-		 *   1. Exiba todas as cores uma abaixo da outra
-		 *   2. A quantidade de cores que o arco-íris tem
-		 *   3. Exiba as cores em ordem alfabética
-		 *   4. Exiba as cores na ordem inversa da que foi informada
-		 *   5. Exiba todas as cores que começam com a letra ”v”
-		 *   6. Remova todas as cores que não começam com a letra “v”
-		 *   Limpe o conjunto
-		 *   Confira se o conjunto está vazio
-		 *   
-		 *   02. 
-		 *   Crie uma classe LinguagemFavorita que possua os atributos nome,
-		 *   anoDeCriacao e ide. Em seguida, crie um conjunto com 3 linguagens
-		 *   e faça um programa que ordene esse conjunto por:
-		 *   1. Ordem de Inserção
-		 *   2. Ordem Natural (nome)
-		 *   3. IDE
-		 *   4. Ano de criação e nome
-		 *   5. Nome, ano de criacao e IDE
-		 *   Ao final, exiba as linguagens no console, um abaixo da outra.
-		 *   
-		 */
+		//Excluindo os elementos que não começam com V
+		Iterator<String> iterator2 = arcoIris.iterator();
+		while (iterator2.hasNext()) {
+			char carac = iterator2.next().charAt(0);
+			if (carac != 'V') iterator2.remove();
+		}
+		
+		//Imprimindo a lista após as modificações
+		System.out.println();
+		System.out.println();
+		System.out.println(arcoIris);
+		
+		//Limpando lista
+		arcoIris.clear();
+		
+		//Confirmando se a lista está vazia
+		System.out.println();
+		System.out.println(arcoIris.isEmpty());
 		
 
+		}
 
 	}
-	
-}
+
